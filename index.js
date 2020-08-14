f8330496f391ef524fb91bf89802a4f6916d189c
geek-dev <gonisetiawan0@gmail.com>
2020-08-14 13:41:07 +0800

    PortFolio New Normal

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000
-app.get('./src/', (req, res) => res.send(index.html));
+app.get('/', (req, res) => res.send(index.html));
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
